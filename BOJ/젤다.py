a7aac0bcce8f3a342f0d620d53cf6b19de4637ff
lunerave <lunerave07@naver.com>
2024-08-06 20:36:02 +0900

    BOJ: 녹색 옷 입은 애가 젤다지?

---
+++ BOJ/젤다.py
@@ -0,0 +1,46 @@
+import heapq
+
+idx = 1
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+while True:
+    n = int(input())
+
+    if n == 0:
+        break
+
+    distance = [[10e9]*n for _ in range(n)]
+
+    graph = []
+
+    q = []
+
+    for _ in range(n):
+        graph.append(list(map(int, input().split())))
+
+
+    heapq.heappush(q, (graph[0][0], 0, 0))
+
+    while q:
+        c, x, y = heapq.heappop(q)
+
+        if c > distance[n-1][n-1]:
+            continue
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if 0 <= nx < n and 0 <= ny < n:
+                nc = c + graph[nx][ny]
+                if distance[nx][ny] > nc:
+                    distance[nx][ny] = nc
+                    heapq.heappush(q, (nc, nx, ny))
+
+    print("Problem " + str(idx) +": " + str(distance[n-1][n-1]))
+    idx += 1
+    
+
+
