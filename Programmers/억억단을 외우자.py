07aa69ab4990d1dfca838c12089ba9738eeb77c2
lunerave <lunerave07@naver.com>
2024-09-24 15:27:26 +0900

    Programmers: 억억단을 외우자; add comment

--- Programmers/억억단을 외우자.py
+++ Programmers/억억단을 외우자.py
@@ -4,17 +4,20 @@ def solution(e, starts):
     
     min_s = min(starts)
     
+    # 약수의 갯수를 구하여 각 numbers에 저장
     for i in range(1, e + 1):
         if i * i <= e:
             numbers[i*i] += 1
         for j in range(i+1, e + 1):
             n = i * j
+            # n > e라면 이미 타겟 넘버를 넘어감
             if n > e:
                 break
             numbers[n] += 2
-        
-    big_numbers = [i for i in range(e+1)]
     
+    big_numbers = [i for i in range(e+1)]
+    # 약수의 갯수가 가장 큰 값 중 작은 값을 기준으로 정렬
+
     for i in range(e-1, min_s-1, -1):
         if numbers[i] < numbers[big_numbers[i+1]]:
             big_numbers[i] = big_numbers[i+1]
